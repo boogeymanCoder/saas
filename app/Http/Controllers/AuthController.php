c76a9865d5f3ac979cc9ef5bb33e981cea918805
John Acaso <johnbryanpitcasoy@gmail.com>
2022-05-16 22:43:00 +0800

    implemented login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -26,6 +27,7 @@ class AuthController extends Controller
         );
 
         $user_data = $request->only(["name", "email", "password"]);
+        $user_data["password"] = bcrypt($user_data["password"]);
         $user = User::create($user_data);
         $token = $user->createToken(env("TOKEN_SECRET"))->plainTextToken;
 
@@ -34,5 +36,42 @@ class AuthController extends Controller
         $tenant->domains()->create(['domain' => $domain . "." . env('HOSTNAME')]);
 
         return response(["success" => true, "data" => ["tenant" => $tenant, "user" => $user, "token" => $token], "errorMessage" => null]);
+    }
+
+    /**
+     * Login a user to get a token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $fields = $request->validate(
+            [
+                "email" => "required|string",
+                "password" => "required|string",
+            ]
+        );
+
+        // Get user with matching email
+        $user = User::where("email", $fields["email"])->first();
+
+        // Check password
+        if (!$user || !Hash::check($fields["password"], $user->password)) {
+            return response(["success" => false, "data" => null, "errorMessage" => "Wrong credentials"]);
+        }
+
+        $token = $user->createToken(env("TOKEN_SECRET"))->plainTextToken;
+
+        $response = [
+            "success" => true,
+            "data" => [
+                "user" => $user,
+                "token" => $token
+            ],
+            "errorMessage" => null
+        ];
+
+        return response($response, 201);
     }
 }
